f9f2d5f1bae1653145f75adc3595f64e4a1d07d8
harsh <harshagarwal26052001@gmail.com>
2022-09-08 01:31:31 +0530

    adding files to heruko

--- index.js
+++ index.js
@@ -118,6 +118,8 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Oh no Something went wrong!!';
     res.status(statusCode).render('error', { err });
 })
-app.listen(3000, () => {
-    console.log("Listening to port 3000!!!");
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}!!!`);
 })
